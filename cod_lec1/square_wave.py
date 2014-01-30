34da31f391bf6a25fec1eb04022c100daa81430d
vimaier <viktor.maier@cern.ch>
2014-01-30 20:14:22 +0100

    Code from coding lecture

--- cod_lec1/square_wave.py
+++ cod_lec1/square_wave.py
@@ -4,20 +4,21 @@ Created on 30.01.2014
 
 <add a description>
 
-.. moduleauthor:: vimaier 
+.. moduleauthor:: vimaier
 
 '''
 
 import sys
 
 import numpy as np
+import timeit
 
 
 #===================================================================================================
 # main()-function
 #===================================================================================================
 def main():
-    
+    measure_time_between_old_and_improved_version()
     return 0
 
 
@@ -25,15 +26,47 @@ def main():
 # helper-functions
 #===================================================================================================
 def calculate_square_wave_for(list_of_t, num_of_osc):
+    result = np.zeros(len(list_of_t))
+    k = np.arange(1, num_of_osc + 1)
+    two_k_minus_one = 2*k - 1 
+
+    for i, t in enumerate(list_of_t):
+        result[i] = np.sum((np.sin(two_k_minus_one*t)) / two_k_minus_one)       
+
+    return (4/np.pi) * result
+
+
+def first_calculate_square_wave_for(list_of_t, num_of_osc):
     result = []
 
     for t in list_of_t:
         s = 0
-        for k in xrange(1, num_of_osc+1):
+        for k in xrange(1, num_of_osc + 1):
             s += (4*np.sin((2*k-1)*t)) / ((2*k-1)*np.pi)
         result.append(s)
 
     return np.array(result)
+
+
+def measure_time_between_old_and_improved_version():
+    setup = """
+import numpy as np
+
+from __main__ import calculate_square_wave_for
+from __main__ import first_calculate_square_wave_for
+
+num_of_osc = 99
+num_of_points = 5
+list_of_t = np.linspace(-np.pi, np.pi, num_of_points)
+"""
+
+    print "Start..."
+    num_of_iterations = 10000
+    print "First version:", timeit.timeit("first_calculate_square_wave_for(list_of_t, num_of_osc)",
+                                            setup, number=num_of_iterations)
+    print "Improved version:", timeit.timeit("calculate_square_wave_for(list_of_t, num_of_osc)",
+                                            setup, number=num_of_iterations)
+    print "Finished..."
 
 
 #===================================================================================================
--- cod_lec1/test_square_wave.py
+++ cod_lec1/test_square_wave.py
@@ -6,21 +6,33 @@ Created on 30.01.2014
 import unittest
 
 import numpy as np
-from cod_lec1.square_wave import calculate_square_wave_for
+import cod_lec1.square_wave as square_wave
+
 
 class Test(unittest.TestCase):
-
 
     def test_square_wave(self):
         assumed_results = np.array([-1.650777599717162E-14, -1.0032151693727007, 0.0, 1.0032151693727007, 1.650777599717162E-14])
 
         num_of_osc = 99
         num_of_points = 5
-        list_of_t = np.linspace(-np.pi, np.pi, 5)
+        list_of_t = np.linspace(-np.pi, np.pi, num_of_points)
 
-        calc_result = calculate_square_wave_for(list_of_t, num_of_osc)
+        calc_result = square_wave.calculate_square_wave_for(list_of_t, num_of_osc)
 
         self.assertTrue(np.allclose(assumed_results, calc_result), "Calculated values are wrong")
+
+    def test_first_square_wave(self):
+        assumed_results = np.array([-1.650777599717162E-14, -1.0032151693727007, 0.0, 1.0032151693727007, 1.650777599717162E-14])
+
+        num_of_osc = 99
+        num_of_points = 5
+        list_of_t = np.linspace(-np.pi, np.pi, num_of_points)
+
+        calc_result = square_wave.first_calculate_square_wave_for(list_of_t, num_of_osc)
+
+        self.assertTrue(np.allclose(assumed_results, calc_result), "Calculated values are wrong")
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test_square_wave']
